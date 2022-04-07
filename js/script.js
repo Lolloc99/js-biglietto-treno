4d5ea7ee4c51407152fd3814d48f321f23681a69
Lolloc99 <lolloc99@yahoo.it>
2022-04-07 14:49:00 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -7,3 +7,45 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 L'output va stampato nella pagina HTML.
 */
+
+// Dati conosciuiti
+const euroxkm = 0.21;
+let sconto = 0;
+let scontoApplicato = 0;
+
+// Dati richiesti all'utente
+const trattaKM = prompt ("Quanti Km vuoi percorrere?");
+console.log(trattaKM);
+
+const etaCliente = prompt ("Quanti anni hai?");
+console.log(etaCliente);
+
+// Calcolo del prezzo del biglietto
+let prezzoBiglietto = euroxkm * trattaKM
+console.log(prezzoBiglietto);
+
+// Calcolo del possibile sconto in base all'età
+if (etaCliente < 18) {
+    sconto = 20;
+    console.log(sconto);
+
+    scontoApplicato = (prezzoBiglietto * sconto) / 100;
+    console.log(scontoApplicato);
+} else if (etaCliente > 65) {
+    sconto = 40;
+    console.log(sconto);
+
+    scontoApplicato = (prezzoBiglietto * sconto) / 100;
+    console.log(scontoApplicato);
+}
+
+// Calcolo del prezzo finale del biglietto con possibile sconto
+
+const prezzoFinale = prezzoBiglietto - scontoApplicato;
+console.log(prezzoFinale);
+
+// Stampa del prezzo su schermo
+
+const printPrezzo = prezzoFinale.toFixed(2);
+
+document.getElementById('prezzo').innerHTML = "Il prezzo del biglietto è: €" + printPrezzo;
